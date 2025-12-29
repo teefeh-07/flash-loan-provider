3860e3307097710d53d6cda611f0a7567e1535a7
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 10:03:47 +0100

    chore: finalize export for useCounter

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,3 +1,5 @@
 import React from 'react';
 
 export const useCounter = (initial = 0) => { const [count, setCount] = React.useState(initial); const inc = () => setCount(c => c + 1); const dec = () => setCount(c => c - 1); return { count, inc, dec }; };
+
+// Exported
