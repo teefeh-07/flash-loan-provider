431583af9a548021b7e100dc6c46ad5fd700a94b
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 10:08:43 +0100

    feat: implement useFocus logic

--- src/hooks/useFocus.js
+++ src/hooks/useFocus.js
@@ -1,2 +1,3 @@
 import React from 'react';
 
+export const useFocus = () => { const [isFocused, setIsFocused] = React.useState(false); const bind = { onFocus: () => setIsFocused(true), onBlur: () => setIsFocused(false) }; return [isFocused, bind]; };
