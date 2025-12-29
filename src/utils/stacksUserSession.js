9205b94028444010f620cee5ae11f2bd9c062bbe
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 09:36:05 +0100

    feat: add stacks user session configuration

---
+++ src/utils/stacksUserSession.js
@@ -0,0 +1,22 @@
+import { AppConfig, UserSession, showConnect } from '@stacks/connect';
+
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+export const userSession = new UserSession({ appConfig });
+
+export function authenticate() {
+  showConnect({
+    appDetails: {
+      name: 'Flash Loan Provider',
+      icon: window.location.origin + '/logo.png',
+    },
+    redirectTo: '/',
+    onFinish: () => {
+      window.location.reload();
+    },
+    userSession,
+  });
+}
+
+export function getUserData() {
+  return userSession.loadUserData();
+}
