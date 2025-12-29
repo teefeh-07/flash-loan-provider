22e92364be489efab45bb3b738ea2596b67442e6
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 09:58:31 +0100

    feat: implement useToggle logic

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,2 +1,3 @@
 import React from 'react';
 
+export const useToggle = (initial = false) => { const [state, setState] = React.useState(initial); const toggle = () => setState(s => !s); return [state, toggle]; };
