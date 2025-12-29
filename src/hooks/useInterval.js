1b45d170d0d10713c09b46d1db931318a10d7d43
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 10:16:24 +0100

    chore: finalize export for useInterval

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,3 +1,5 @@
 import React from 'react';
 
 export const useInterval = (fn, ms) => React.useEffect(() => { const t = setInterval(fn, ms); return () => clearInterval(t); }, [fn, ms]);
+
+// Exported
