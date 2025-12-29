cf56175e2f86fa4a6f8000a5a3e3183446240731
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 09:39:10 +0100

    feat: integrate ConnectWallet into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ConnectWallet from './components/ConnectWallet';
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Flash Loan Provider</h1>
+        <ConnectWallet />
       </header>
       <main>
         <p>Welcome to the Flash Loan Provider on Stacks.</p>
