45f9953a6d635c4908ae6de7222a3927d9d13503
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 10:05:04 +0100

    feat: implement useHover logic

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,2 +1,3 @@
 import React from 'react';
 
+export const useHover = () => { const [isHovered, setIsHovered] = React.useState(false); const bind = { onMouseEnter: () => setIsHovered(true), onMouseLeave: () => setIsHovered(false) }; return [isHovered, bind]; };
